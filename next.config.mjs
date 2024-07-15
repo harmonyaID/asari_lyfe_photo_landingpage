e62e0512a4a64754c597adf4b08e51e3db4cd53c
LevineLighto <hello@irimold.web.id>
2024-07-15 21:53:12 +0800

    dependecy: Install bootstrap

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+import CopyPlugin from 'copy-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.plugins.push(
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+                        to: "../public/js/bootstrap.js"
+                    }
+                ]
+            })
+        )
+
+        return config
+    }
+};
 
 export default nextConfig;
