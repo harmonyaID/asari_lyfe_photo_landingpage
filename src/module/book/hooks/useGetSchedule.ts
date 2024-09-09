9d790a10bc8b7bb50779ed67730df2dffe551983
LevineLighto <hello@irimold.web.id>
2024-09-09 19:39:30 +0700

    fix(Schedule): Only get schedule with time > now

--- src/module/book/hooks/useGetSchedule.ts
+++ src/module/book/hooks/useGetSchedule.ts
@@ -1,7 +1,15 @@
 import { useSwr } from "@/hooks/swr"
 import { Schedule } from "../types"
 import { GetSchedule } from "../urls"
+import { useMemo } from "react"
 
 export const useGetSchedule = (locationId?: number | string, date?: string) => {
-    return useSwr<Schedule[]>(!locationId || !date ? null : GetSchedule, { locationId, date })
+    const filter = useMemo(() => ({
+        locationId,
+        date,
+        notExpired  : true,
+        isActive    : true,
+    }), [locationId, date])
+    
+    return useSwr<Schedule[]>(!locationId || !date ? null : GetSchedule, filter)
 }
