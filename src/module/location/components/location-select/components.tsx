19e0c8ca7c01a7a029e44497b17006eac469ee15
LevineLighto <hello@irimold.web.id>
2025-08-05 23:00:42 +0700

    fix(Select): make sure the selected item is fetched from db

--- src/module/location/components/location-select/components.tsx
+++ src/module/location/components/location-select/components.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC, useMemo, useState } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
 import { LocationSelectProps } from "./props";
 import { useGetLocation } from "../../hooks";
 import { Select, SelectOptionType } from "@/components/inputs/select";
@@ -8,11 +8,14 @@ import { SearchableSelect } from "@/components/inputs/searchable-select";
 
 export const LocationSelect : FC<LocationSelectProps> = ({
     className = '',
+    value,
     ...props
 }) => {
     const [filter, setFilter] = useState<Record<string, any>>({
         search  : '',
         isActive: '1',
+        selectedId  : !Array.isArray(value) && typeof value == 'number' ?  value : undefined,
+        selectedIds : Array.isArray(value) && value.length && typeof value[0] == 'number' ? value.join(',') : undefined
     })
 
     const { data, isLoading } = useGetLocation(filter)
@@ -24,12 +27,22 @@ export const LocationSelect : FC<LocationSelectProps> = ({
         }))
     }
 
+    
+    useEffect(() => {
+        setFilter((prevFilter) => ({
+            ...prevFilter,
+            selectedId  : !Array.isArray(value) && typeof value == 'number' ?  value : undefined,
+            selectedIds : Array.isArray(value) && value.length && typeof value[0] == 'number' ? value.join(',') : undefined
+        }))
+    }, [value])
+
     return (
         <SearchableSelect
             name="locationId"
             label="Location"
             className={className}
             data={data?.result || []}
+            value={value}
             itemLabelKey="name"
             onSearch={handleSearch}
             loading={isLoading}
