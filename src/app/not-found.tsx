81b8fd8144b3c21eaa80ba9bfc0508c525304b94
LevineLighto <ahmad.dlomiri00@gmail.com>
2026-08-31 21:36:27 +0700

    fix(NotFound): Adjust not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,21 +3,23 @@ import { ErrorIcon } from "@/components/icons";
 import { FC } from "react";
 
 const NotFound : FC = () => (
-    <section>
-        <div className="pb-3 mb-5">
-            <ErrorIcon/>
-        </div>
-        <h1 className="pb-2 mb-1 page-title bold">
-            404 PAGE NOT FOUND
-        </h1>
-        <p className="fw-light pb-5 mb-1">
-            We{"'"}re sorry, the page you are looking for does not exist in our database!
-        </p>
-        <div className="d-grid">
-            <LinkButton href="/" pill>
-                Back to Home
-            </LinkButton>
-        </div>
+    <section className="vw-100 vh-100 pt-5">
+        <main className="container pt-5">
+            <div className="pt-lg-5">
+                <ErrorIcon
+                    className="mb-5"
+                />
+                <h1 className="pb-2 mb-1 page-title bold">
+                    404 PAGE NOT FOUND
+                </h1>
+                <p className="fw-light pb-5 mb-1">
+                    We{"'"}re sorry, the page you are looking for does not exist in our database!
+                </p>
+                <LinkButton href="/" pill>
+                    Back to Home
+                </LinkButton>
+            </div>
+        </main>
     </section>
 )
 
